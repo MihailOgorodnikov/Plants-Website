8edb54ccb9c4bce4b6d764be39a2eb449e671329
Mihail <mihail-79999@mail.ru>
2022-07-23 14:53:59 +0300

    toggleCart

---
+++ js/toggleCartStatus.js
@@ -0,0 +1,15 @@
+function toggleCartStatus() {
+
+    const cartWrapper = document.querySelector('.cart-wrapper');
+    const cartEmptyBadge = document.querySelector('[data-cart-empty]');
+
+    if (cartWrapper.children.length > 0) {
+        console.log('FULL');
+        cartEmptyBadge.classList.add('none');
+       
+    } else {
+        console.log('EMPTY');
+        cartEmptyBadge.classList.remove('none');        
+    }
+
+}
